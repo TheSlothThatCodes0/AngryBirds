b4690bfa99a5554c6123f7e9f7ba24748c983cb5
Siddharth Yadav <siddharth23525@iiitd.ac.in>
2024-10-17 23:37:55 +0530

    added the splashScreen

--- core/src/main/java/com/angryBirds/Main.java
+++ core/src/main/java/com/angryBirds/Main.java
@@ -2,31 +2,50 @@ package com.angryBirds;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private float WORLD_WIDTH = 1024;  // Virtual width
+    private float WORLD_HEIGHT = 600;  // Virtual height
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("splashScreen.png");
+
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        viewport.apply();
+
+        camera.position.set(WORLD_WIDTH/2, WORLD_HEIGHT/2, 0);
 
         Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
-
     }
 
     @Override
     public void render() {
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);
         batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+        camera.position.set(WORLD_WIDTH/2, WORLD_HEIGHT/2, 0);
     }
 
     @Override
