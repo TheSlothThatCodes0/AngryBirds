7feb52fce7920d2695a8da1969ea60da4a9329e1
Shaman <knock.knock.noco@gmail.com>
2024-10-17 22:44:48 +0530

    made the application full screen

--- core/src/main/java/com/angryBirds/Main.java
+++ core/src/main/java/com/angryBirds/Main.java
@@ -2,7 +2,7 @@ package com.angryBirds;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+//import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
