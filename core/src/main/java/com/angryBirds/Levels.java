ffd0e1fba8733fc6a599f9d48f49dfbf5ea2f34e
Shaman <knock.knock.noco@gmail.com>
2024-10-19 21:58:04 +0530

    Added bird, pig class and some attributes to Levels.java

--- core/src/main/java/com/angryBirds/Levels.java
+++ core/src/main/java/com/angryBirds/Levels.java
@@ -1,4 +1,20 @@
 package com.angryBirds;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
 public class Levels {
+    private Main game;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private float WORLD_WIDTH = 1920;
+    private float WORLD_HEIGHT = 1080;
+
+    // Textures
+    private Texture backgroundTexture;
+    private Texture ground_shape;
+    private Texture launcher1;
+    private Texture launcher2;
+
 }
---
+++ core/src/main/java/com/angryBirds/Pig.java
@@ -0,0 +1,4 @@
+package com.angryBirds;
+
+public class Pig {
+}
